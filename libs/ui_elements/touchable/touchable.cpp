3edfdd6becbfcacdb2daea9867dac6f5637a3ef2
Tanasescu Darius <128938124+the-nightraven@users.noreply.github.com>
2024-11-20 04:07:06 +0200

    created source and header for a touchable element

---
+++ libs/ui_elements/touchable/touchable.cpp
@@ -0,0 +1,23 @@
+/*
+###########################################################
+
+@file: touchable.cpp
+
+@author: Tanasescu Darius
+
+@Licence: Apache2.0
+
+@Copyleft the-nightraven, product is open source and has
+absolutely no warranty, you are free to copy, distribute
+and change, but not for commercial use
+
+@Revision: InfDev
+
+@Description: source file that defines functionality for
+an element that can be interacted with via mouse events
+such as: "Click", "Hover" and "Stop Hover"
+
+###########################################################
+*/
+
+#include "touchable.h"
