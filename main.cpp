fe2cab6b0d0f159aaf751c8a104b3edf145b01d1
Tanasescu Darius <128938124+the-nightraven@users.noreply.github.com>
2025-02-05 03:47:16 +0200

    try again

--- main.cpp
+++ main.cpp
@@ -19,12 +19,20 @@ and change, but not for commercial use
 */
 
 #include "app.h"
+
+#ifndef DEBUG
+#include <windows.h>
+#define SDL_MAIN_HANDLED
+#endif
+
 #define MAIN_TAG       "MAIN"
 
 //external definitions
 void system_print(char* str) {
     printf("%s\n", str);
 }
+
+#if DEBUG
 
 int main(int argc, char* args[]) {
     G_STATUS status;   
@@ -50,3 +58,34 @@ int main(int argc, char* args[]) {
 
     return status;
 }
+
+#else
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow) {
+    SDL_SetMainReady();
+
+    G_STATUS status;
+    status = app_init();
+
+    if(status == G_STATUS_FAIL) {
+        log_error(MAIN_TAG, "Cannot init app", G_STATUS_FAIL);
+        return G_STATUS_FAIL;
+    }
+
+    status = app_loop();
+    if(status == G_STATUS_FAIL) {
+        log_error(MAIN_TAG, "Loop returned errors", -1);
+    }
+    log_info(MAIN_TAG, "Successful loop exit");
+
+    //TODO: verbose deinit
+    status = app_deinit();
+    if(status == G_STATUS_FAIL) {
+        log_error(MAIN_TAG, "Error on deinit", -1);
+    }
+    log_info(MAIN_TAG, "Successful deinit");
+
+    return status;
+}
+
+#endif
